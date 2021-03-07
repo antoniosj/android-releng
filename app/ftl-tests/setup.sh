5a155c64c9435ec6b23e578814a1e2a38ed1b8e7
Antonio Silva <antonio.silvajr23@gmail.com>
2021-03-07 13:59:04 -0300

    WIP: gcloud test  api

--- app/ftl-tests/setup.sh
+++ app/ftl-tests/setup.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-if [ "$GCLOUD" = "" ]; then
-  echo "GCLOUD env variable is empty. Exiting."
+if [ "$GCLOUD_SERVICE_KEY" = "" ]; then
+  echo "GCLOUD_SERVICE_KEY env variable is empty. Exiting."
   exit 1
 fi
 
 # Export to secrets file
-echo $GCLOUD | base64 -di > client-secret.json
-
-# Set project ID
-gcloud config set project android-releng
+echo $GCLOUD_SERVICE_KEY | base64 -di > client-secret.json
 
 # Auth account gcloud api
 gcloud auth activate-service-account firebase-adminsdk-o1bzs@android-releng.iam.gserviceaccount.com --key-file client-secret.json
+
+# Set project ID
+gcloud --quiet config set project android-releng
+
+gcloud firebase test android run \
+    --type instrumentation \
+    --app app/build/outputs/apk/debug/app-debug.apk \
+    --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
+    --device model=Nexus6P,version=27,locale=en_US,orientation=portrait \
+    --timeout 30m
 
 # Delete secret
 rm client-secret.json
