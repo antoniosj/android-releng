e2bf064aab617098188d068b287094607b04765a
Antonio Silva <antonio.silvajr23@gmail.com>
2021-03-07 14:48:12 -0300

    Update config yml to split ftl test and config

--- app/ftl-tests/setup.sh
+++ app/ftl-tests/setup.sh
@@ -14,12 +14,5 @@ gcloud auth activate-service-account firebase-adminsdk-o1bzs@android-releng.iam.
 # Set project ID
 gcloud --quiet config set project android-releng
 
-gcloud firebase test android run \
-    --type instrumentation \
-    --app app/build/outputs/apk/debug/app-debug.apk \
-    --test app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
-    --device model=Nexus6P,version=27,locale=en_US,orientation=portrait \
-    --timeout 30m
-
 # Delete secret
 rm client-secret.json
